995484e1bbc3ab4b9876a126e739467f59ef0664
vanesa.lido <vanesa.lido@gmail.com>
2023-11-08 16:14:04 +0100

    intento de req1 de otro modo e inicio req2

--- actividad2.js
+++ actividad2.js
@@ -1,4 +1,3 @@
-console.log("hola")
 /*
     //Creamos nodo tipo elemento
     let labelTamano = document.createElement("label");
@@ -13,6 +12,7 @@ console.log("hola")
     document.getElementById("tamanos").appendChild(labelTamano);
  */   
 
+//Intento Estefanía
 /*
 const URL_DESTINO = "http://localhost:5500/AE2_AJAX/"; 
 const RECURSO = "pizza.json"; 
@@ -41,10 +41,12 @@ function cargarPagina() {
         cargarIngredientes(datos.ingredientes);
     });
 }*/
+
+
 window.addEventListener("load", function () {
     // Realizo una solicitud AJAX para obtener los datos del servidor de la forma que nos ha enseñado felix no lo consigo
     const xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", "pizza.json", true); 
+    xmlhttp.open("GET", "actividad2.json", true); 
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
             const data = JSON.parse(xmlhttp.responseText);
@@ -54,6 +56,41 @@ window.addEventListener("load", function () {
     };
     xmlhttp.send();
 });
+
+//Intento VANESA
+/*
+const URL_DESTINO = "http://localhost:5501/AE2_AJAX_GRUPO8/" //http://127.0.0.1:5501/
+const RECURSO = "actividad2.json"
+
+    function cargarDatosDesdeJSON() {
+
+        let xmlHttp = new XMLHttpRequest()
+
+        xmlHttp.onreadystatechange = function () {
+            console.log(this.readyState)
+            if (this.readyState == 4) {
+                if (this.status == 200) {
+                    cargarPagina(this.responseText);//Obtenemos el valor en texto
+                } else {
+                    alert("Error al cargar los datos desde el servidor.")
+                }
+            }
+        }
+
+        xmlHttp.open('GET', URL_DESTINO + RECURSO, true)
+        xmlHttp.send(null)
+    }
+
+    function cargarPagina() {
+        cargarDatosDesdeJSON(URL_DESTINO, function (data) {
+            const datos = JSON.parse(data);
+            cargarTamanos(datos.tamanos);
+            cargarIngredientes(datos.ingredientes);
+        });
+    }
+*/
+
+// Función para cargar tamaños dinámicamente
 function cargarTamanos(tamano) {
     const tamanosDiv = document.getElementById("tamanos-pizza");
     tamano.forEach((tamano) => {
@@ -65,13 +102,16 @@ function cargarTamanos(tamano) {
 
         const label = document.createElement("label");
         label.setAttribute("for", tamano.id);
-        label.textContent = `${tamano.nombre} - ${tamano.precio}€`;
+        label.textContent = `${tamano.nombre} - ${tamano.precio}€`;//expandir variable
 
         tamanosDiv.appendChild(radio);
         tamanosDiv.appendChild(label);
         tamanosDiv.appendChild(document.createElement("br"));
     });
+
+    tamanosDiv.appendChild(document.createElement("br"));//He añadido este br
 }
+
 // Función para cargar ingredientes dinámicamente
 function cargarIngredientes(ingredientes) {
     const ingredientesDiv = document.getElementById("ingredientes-pizza");
@@ -89,4 +129,98 @@ function cargarIngredientes(ingredientes) {
         ingredientesDiv.appendChild(label);
         ingredientesDiv.appendChild(document.createElement("br"));
     });
+
+    ingredientesDiv.appendChild(document.createElement("br"));//He añadido este br
 }
+
+//REQUERIMIENTO 2
+//Cuando haga click se calcula el precio y se muestra
+window.addEventListener("click", function () {
+    // Realizo una solicitud AJAX para obtener los datos del servidor de la forma que nos ha enseñado felix no lo consigo
+    const xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", "actividad2.json", true); 
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            const data = JSON.parse(xmlhttp.responseText);
+            calcularPrecio(data.tamanos, data.ingredientes);
+            //cargarTamanos(data.tamanos);
+            //cargarIngredientes(data.ingredientes);
+        }
+    };
+    xmlhttp.send();
+});
+
+//Llamada a AJAX para pedir precios y sumar lo seleccionado
+function calcularPrecio(){
+    let resultado = 0;
+    let radios = document.getElementsByName("tamano");
+    for(radio of radios){
+        if(tamano.checked){
+            continue
+        }       
+    }
+
+}
+
+/*
+window.onload = function () {
+    cargarPagina();
+}*/
+
+/*
+window.onload = function (){
+
+    
+    function calcularPrecio() {
+        const nombre = formulario.elements.nombre.value;
+        const direccion = formulario.elements.direccion.value;
+        const telefono = formulario.elements.telefono.value;
+        const email = formulario.elements.email.value;
+        const tamano = formulario.elements.tamano.value;
+        const ingredientes = formulario.elements['ingredientes[]'];
+    
+        let precioBase = 0;
+    
+        switch (tamano) {
+            case "pequena":
+                precioBase = 5;
+                break;
+            case "mediana":
+                precioBase = 10;
+                break;
+            case "grande":
+                precioBase = 15;
+                break;
+        }
+    
+        let precioIngredientes = 0;
+    
+        for (let i = 0; i < ingredientes.length; i++) {
+            if (ingredientes[i].checked) {
+                precioIngredientes += 1;
+            }
+        }
+
+        if (nombre == "" || direccion == "" || telefono == "" || email == ""){
+            alert("Faltan datos");
+            return;
+        }
+        
+        if (precioBase === 0) {
+            alert("Debes seleccionar un tamaño de pizza.");
+            return;
+        }
+    
+        if (precioIngredientes === 0) {
+            alert("Debes seleccionar al menos un ingrediente.");
+            return;
+        }
+
+        const precioTotal = precioBase + precioIngredientes;
+        document.getElementById("resultado").innerHTML = `Tamaño: ${tamano}<br>Ingredientes: ${precioIngredientes}€<br>Precio total: ${precioTotal}€`;
+    }
+        
+
+    boton.onclick = calcularPrecio;
+}
+*/
